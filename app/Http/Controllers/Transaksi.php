9918a31d24ae41f08dfd549a92084e88c9064ec5
ramasakti <ramasakti1337@gmail.com>
2023-04-02 14:45:58 +0700

    View Transaksi

--- app/Http/Controllers/Transaksi.php
+++ app/Http/Controllers/Transaksi.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class Transaksi extends Controller
 {
-    //
+    public function index()
+    {
+        return view('transaksi.index', [
+            'title' => 'Transaksi',
+            'navactive' => 'transaksi'
+        ]);
+    }
+
+    public function transaksi(Request $request)
+    {
+        
+    }
 }
