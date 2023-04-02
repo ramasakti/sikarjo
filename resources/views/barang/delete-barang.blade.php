d2b9c5f7c549b0319a31175370f8153946474117
ramasakti <ramasakti1337@gmail.com>
2023-04-02 14:39:08 +0700

    Modal Box Delete Barang

---
+++ resources/views/barang/delete-barang.blade.php
@@ -0,0 +1,13 @@
+<div id="delete-barang-{{ $barang->id_barang }}" class="uk-flex-top" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+        <button class="uk-modal-close-default" type="button" uk-close></button>
+        <p class="uk-text-right">
+            <h5 class="uk-margin-small">Yakin Hapus {{ $barang->nama_barang }}?</h5>
+            <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
+            <form action="/barang/delete" method="post">
+                <input type="hidden" name="id_barang" value="{{ $barang->id_barang }}">
+                <button class="uk-button uk-button-primary" type="submit">Ya</button>
+            </form>
+        </p>
+    </div>
+</div>
